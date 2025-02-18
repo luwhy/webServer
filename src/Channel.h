2212d6c7c5b8cf30fad01f0a703e8f2005f1df58
luwhy <1023412173@qq.com>
2025-02-18 10:10:25 +0800

    commit

---
+++ src/Channel.h
@@ -0,0 +1,47 @@
+#ifndef __CHANNEL_H
+#define __CHANNEL_H
+#include <functional>
+#include "sylar/log.h"
+#include "EventLoop.h"
+namespace webs
+{
+    class Channel
+    {
+    public:
+        typedef std::function<void()> EventCallback;
+        Channel(EventLoop *loop, int fd);
+        Channel(Channel &c) = delete;
+
+        void handleEvent();
+        void setReadCallback(const EventCallback &cb) { this->readCallback_ = cb; }
+        void setWriteCallback(const EventCallback &cb) { this->writeCallback_ = cb; }
+        void setErrorCallback(const EventCallback &cb) { this->errorCallback_ = cb; }
+
+        int fd() const { return fd_; }
+        int events() const { return events_; }
+        void set_revents(int revt) { this->revents_ = revt; }
+        bool isNoneEvent() const { return events_ == kNoneEvent; }
+        void enableReading()
+        {
+            events_ |= kReadEvent;
+            update();
+        }
+
+    private:
+        void update();
+        static const int kNoneEvent;
+        static const int kReadEvent;
+        static const int kWriteEvent;
+
+        EventLoop *loop_;
+        const int fd_;
+        int events_;
+        int revents_;
+        int index_; // used by poller
+
+        EventCallback readCallback_;
+        EventCallback writeCallback_;
+        EventCallback errorCallback_;
+    };
+}
+#endif
